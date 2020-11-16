40810a3e8d75f9c50a25cdccf2d50892481c1784
Karina Guizelli <karina.guizelli@gmail.com>
2020-11-15 22:02:02 -0300

    criando movimentos da cobrinha

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
+
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "pink";
     context.fillRect(0, 0, 16 * box, 16 * box);   
 }
 
@@ -20,5 +22,25 @@ function criarCobrinha() {
     
 }
 
-criarBG();
-criarCobrinha(); 
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha(); 
+    
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead); 
+}
+
+let jogo = setInterval(iniciarJogo, 100);
