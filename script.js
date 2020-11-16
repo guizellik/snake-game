fdbac26c133f79bb6b61cc112984907de5abcc8f
Karina Guizelli <karina.guizelli@gmail.com>
2020-11-15 22:56:53 -0300

    realizando ultimos ajustes e aplicando estilos

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let food = {
 
 
 function criarBG() {
-    context.fillStyle = "green";
+    context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);   
 }
 
@@ -27,7 +27,7 @@ function criarCobrinha() {
 }
 
 function drawFood() {
-    context.fillStyle = "pink";
+    context.fillStyle = "#F600FA";
     context.fillRect(food.x, food.y, box, box);
 }
 
@@ -46,6 +46,13 @@ function iniciarJogo() {
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i=1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert("Game Over :(");
+        }
+    }
 
     criarBG();
     criarCobrinha();
